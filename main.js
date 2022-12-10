5fd49986ed78f73c73ba82dfee95da38b1fd5173
victor <victor@victors-MacBook-Air.local>
2022-12-10 13:52:42 -0500

    build the deck of cards

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+//console.log("hello world!");
+// building deck
+const values = ["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "K", "Q", "J"]
+const suits = ["c", "d", "h", "s"]
+let deck = [];
+function buildDeck() {
+    values.forEach(function(value) {
+        suits.forEach(function(suit) {
+            deck.push(suit + value);
+
+        })
+    })
+
+    
+    console.log(deck)
+}
+buildDeck()
