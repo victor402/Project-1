9886e15045ac8d954dccdefb052a517a87b1dc5e
victor <victor@victors-MacBook-Air.local>
2022-12-12 21:37:50 -0500

    gave the dealer back a card and set the size of image tag in style.css

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function buildDeck() {
     })
 
     
-    console.log(deck, deck.length)
+    //console.log(deck, deck.length)
 }
 buildDeck()
 
@@ -21,11 +21,30 @@ let shufDeck = [];
 function buildshufDeck() {
     for (let i = 0; i < 52; i++) {
         let number = 0;
-         number = Math.floor(Math.random()* deck.length)
-         let remove = deck.splice(number, 1) // we taking it out so it doesn't repeat itself.
+         number = Math.floor(Math.random()* deck.length)// this line create a random number(cards)
+         let remove = deck.splice(number, 1) // we taking it out so it doesn't repeat itself.so with splice it remove 1 card and replace 
         shufDeck.push(remove[0]); // to acces a specific card of array then ypu need a square.
         
     }
-console.log(shufDeck)
+//console.log(shufDeck)
 }
 buildshufDeck()
+
+// // dealing the dealer's hand and player's hand
+// const dealerHand = [];
+// function buidDealerHand() {
+// for (let i = 0; i < 2; i++) {
+//     dealerHand.push(buildDeck)
+// }
+// console.log(dealerHand)
+// }
+// buidDealerHand();
+// //const playerHand = [];
+// //dealerHand.push(buildshufDeck);
+// //dealerHand.push(buildshufDeck);
+// //playerHand.push(buildshufDeck);
+// //playerHand.push(buildshufDeck);
+
+// //console.log(dealerHand);
+// //console.log(playerHand);
+
